81a182f6d4073adcae3a520e4d16766d44fde62d
carlton <carltonmbaja0@gmail.com>
2023-02-06 23:37:17 -0800

    Add templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,6 +89,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #         'PORT': os.environ["PGPORT"],
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 # Password validation
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from portfolio.models import PreviousWork
+from . import forms
+from portfolio.forms import NewContact
 # Create your views here.
+
+def index(request):
+    index = NewContact()
+
+    if request.method == 'POST':
+        index = NewContact(request.POST)
+
+        if index.is_valid():
+            index.save(commit=True)
+            # return index(request)
+        else:
+            print('ERROR FORM INVALID')
+    return render(request, 'portfolio/index.html', {'form':index})
+    
+
+def access(request):
+    work_list = PreviousWork.objects.all()
+    # work_dict = {'access_records':work_list}
+    return render(request, 'portfolio/index.html', {'work_list': work_list})
